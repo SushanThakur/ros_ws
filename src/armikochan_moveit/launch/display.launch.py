c3c8ec07aaf7e7ceff4257b41e47afa4e6022105
Sushant Thakur <mail@sushant.uk>
2025-10-09 18:41:34 +0545

    added test launch file for armikochan_moveit and fixed dual servo_node and rviz2 node issue

--- src/armikochan_moveit/launch/display.launch.py
+++ src/armikochan_moveit/launch/display.launch.py
@@ -20,10 +20,6 @@ def generate_launch_description():
         description="RViz configuration file",
     )
 
-    launch_as_standalone_node = LaunchConfiguration(
-        "launch_as_standalone_node", default="false"
-    )
-
     # servo_params = os.path.join(
     #     get_package_share_directory("armikochan_moveit"),
     #     "config", "servo.yaml"
@@ -90,7 +86,7 @@ def generate_launch_description():
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
-        name="rviz2",
+        # name="rviz2",
         # output="log",
         output="screen",
         arguments=["-d", rviz_config],
@@ -110,7 +106,7 @@ def generate_launch_description():
         name="static_transform_publisher",
         output="log",
         # arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "panda_link0"],
-        arguments=["0", "0", "0", "0", "0", "0", "world", "base_link"],
+        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "map", "base_link"],
 
     )
 
@@ -161,27 +157,10 @@ def generate_launch_description():
         arguments=["end_effector_controller", "-c", "/controller_manager"],
     )
 
-    # servo_node = Node(
-    #     package="moveit_servo",
-    #     executable="servo_node",
-    #     name="servo_node",
-    #     parameters=[
-    #         # servo_params_file,
-    #         servo_params,
-    #         planning_group_name,
-    #         acceleration_filter_update_period,
-    #         moveit_config.robot_description,
-    #         moveit_config.robot_description_semantic,
-    #         moveit_config.robot_description_kinematics,
-    #         moveit_config.joint_limits,
-    #     ],
-    #     output="screen",
-    # )
-
     servo_node = launch_ros.actions.Node(
         package="moveit_servo",
         executable="servo_node",
-        name="servo_node",
+        # name="servo_node",
         parameters=[
             servo_params,
             planning_group_name,
@@ -192,14 +171,12 @@ def generate_launch_description():
             moveit_config.joint_limits,
         ],
         output="screen",
-        # condition=IfCondition(launch_as_standalone_node),
     )
 
     return LaunchDescription(
         [
             rviz_config_arg,
             ros2_control_hardware_type,
-            rviz_node,
             static_tf_node,
             robot_state_publisher,
             move_group_node,
@@ -207,6 +184,8 @@ def generate_launch_description():
             joint_state_broadcaster_spawner,
             arm_controller_spawner,
             gripper_controller_spawner,
+
+            rviz_node,
             servo_node,
         ]
     )
